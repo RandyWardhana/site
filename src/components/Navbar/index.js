03f6804c467aa2cab363e3b989018b873f527c03
RandyWardhana <randywardhana37@gmail.com>
2020-08-06 20:12:37 +0700

    Update Navbar Section

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,105 +2,118 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 import Link from 'next/link'
 
-const Name = styled.h1`
-  color: #FFFFFF;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 40px;
-  margin: 8px 0px 0px;
-`
+import { Menu } from '../Svg'
 
-const Profession = styled.span`
-  color: #888A8E;
-  display: block;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 20px;
-`
-
-const Greeting = styled(Profession)`
-  font-weight: 300
-`
-
-const Links = styled.ul`
+const Container = styled.div`
+  background: ${({ theme }) => theme.background};
+  box-shadow: 0 2px 4px 0 rgba(136, 138, 142, 0.15);
   display: flex;
-  list-style-type: none;
-  margin: 0px;
-  margin-top: 24px;
-  padding: 0px;
+  position: fixed;
+  margin: 0;
+  padding: 0;
+  transition: ${({ theme }) => theme.transition};
+  width: 100%;
+  z-index: 999;
+`
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 16px;
+  width: 100%;
+
+  @media (max-width: 576px) {
+    max-width: 540px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
 `
 
-const StyledLink = styled.li`
-  align-self: flex-start;
-  flex: none;
-  margin: 0px 8px;
-  order: 1;
-  &:first-child {
-    margin-left: 0px;
-  }
-  &:last-child {
-    margin-right: 0px;
-  }
+const MenuContainer = styled.div`
+  align-items: center;
+  display: flex;
+`
+
+const StyledLink = styled.p`
+  margin-block-end: 0;
+  margin-block-start: 0;
+  
   a {
-    color: #888A8E;
+    color: ${({ theme }) => theme.secondary};
     display: inline-block;
     font-style: normal;
-    font-weight: normal;
+    font-weight: 400;
     font-size: 14px;
-    min-width: 43px;
+    margin-right: 16px;
     text-decoration: none;
-    transition: all .15s ease-in-out;
+    transition: ${({ theme }) => theme.transition};
+
     &:hover {
-      color: #FFFFFF;
+      color: ${({ theme }) => theme.primary};
     }
   }
+  ${(props) => props.lastChild && css`
+    a {
+      margin-right: 0
+    }
+  `}
   ${(props) => props.active && css`
     a {
-      color: #FFFFFF;
+      color: ${({ theme }) => theme.primary};
       font-weight: 500;
     }
   `}
 `
 
-const Greetings = () => {
-  let d = new Date()
-  let time = d.getHours()
-
-  switch (true) {
-    case time >= 4 && time <= 10:
-      return 'Selamat Pagi!'
-    case time > 10 && time <= 14:
-      return 'Selamat Siang!'
-    case time > 14 && time <= 17:
-      return 'Selamat Sore!'
-    default:
-      return 'Selamat Malam!'
-  }
-}
-
-const Navbar = ({ currentPage }) => (
-  <nav>
-    <Greeting>{Greetings()}</Greeting>
-    <Name>RANDY WARDHANA</Name>
-    <Profession>Frontend Engineer</Profession>
-    <Links>
-      <StyledLink active={currentPage === 'HOME'}>
-        <Link href="/" passHref>
-          <a>Home</a>
-        </Link>
-      </StyledLink>
-      <StyledLink active={currentPage === 'WORKS'}>
-        <Link href="/works" passHref>
-          <a>Works</a>
-        </Link>
-      </StyledLink>
-      <StyledLink active={currentPage === 'ABOUT'}>
-        <Link href="/about" passHref>
-          <a>About</a>
-        </Link>
-      </StyledLink>
-    </Links>
-  </nav>
+const Navbar = ({ currentPosition, size, theme }) => (
+  <>
+    <nav>
+      <Container>
+        <Content>
+          <MenuContainer>
+            <StyledLink active={currentPosition === 'HOME'} lastChild={false}>
+              <Link href="/" passHref>
+                <a style={{ color: theme.primary, fontWeight: '700' }}>RANDY</a>
+              </Link>
+            </StyledLink>
+          </MenuContainer>
+          {size.width <= 576 && (
+            <Menu space={16} fill={theme.primary} />
+          )}
+          {size.width > 576 && (
+            <div style={{ display: 'flex' }}>
+              <StyledLink active={currentPosition === 'PROJECT'} lastChild={false}>
+                <Link href="/#project" passHref>
+                  <a>Project</a>
+                </Link>
+              </StyledLink>
+              <StyledLink active={currentPosition === 'SKILLS'} lastChild={false}>
+                <Link href="/#skills" passHref>
+                  <a>Skills</a>
+                </Link>
+              </StyledLink>
+              <StyledLink active={currentPosition === 'CONTACT'} lastChild={true}>
+                <Link href="/#contact" passHref>
+                  <a>Contact</a>
+                </Link>
+              </StyledLink>
+            </div>
+          )}
+        </Content>
+      </Container>
+    </nav>
+  </>
 )
 
 export default Navbar
