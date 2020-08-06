c672d0d6e2420f608915ce3b3e7b6537d37bb293
RandyWardhana <randywardhana37@gmail.com>
2020-08-06 20:15:00 +0700

    Create Theme file for Dual Theme Function

---
+++ src/utils/Themes.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+  background: '#FFFFFF',
+  primary: '#2F3136',
+  secondary: '#8B8B8B',
+  font: "'Poppins', sans-serif",
+  transition: 'all .3s ease-in-out'
+}
+
+export const darkTheme = {
+  background: '#2F3136',
+  primary: '#FFFFFF',
+  secondary: '#8B8B8B',
+  font: "'Poppins', sans-serif",
+  transition: 'all .3s ease-in-out'
+}
