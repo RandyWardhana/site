c6cf3a6253081c2f4821007a7903e2f995e83867
RandyWardhana <randywardhana37@gmail.com>
2020-08-08 18:05:12 +0700

    config firebase

---
+++ src/utils/Firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
+
+if(!firebase.apps.length) firebase.initializeApp(firebaseConfig)
+else firebase.app()
+
+export default firebase
