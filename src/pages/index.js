22d0838f8a2afb0ce363564dadead3255af85ed7
RandyWardhana <randywardhana37@gmail.com>
2020-08-08 18:09:00 +0700

    get data from firebase with SSR getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,24 +9,36 @@ import Footer from '../components/Footer'
 import Layout from '../layout'
 
 import useWindowSize from '../utils/resizeWindowSize'
+import { getProjects, getHeader } from '../utils/FirebaseRequest'
 
 const Index = (props) => {
   const size = useWindowSize()
-
   return (
     <>
       <Navbar currentPosition={'HOME'} size={size} theme={props.theme} toggle={props.toggle} />
       <Layout>
         <div style={{ margin: '60px auto 0', padding: size.width <= 768 ? '0 32px' : 0 }}>
-          <Header size={size} theme={props.theme} toggle={props.toggle} />
-          <Project size={size} />
-          <Skills />
+          <Header data={props.responseHeader} size={size} theme={props.theme} />
+          <Project data={props.responseProjects} size={size} />
+          <Skills theme={props.theme} />
           <Contact size={size} theme={props.theme} />
           <Footer size={size} theme={props.theme} />
         </div>
       </Layout>
     </>
   )
+}
+
+export async function getStaticProps () {
+  const responseHeader = await getHeader()
+  const responseProjects = await getProjects()
+  
+  return {
+    props: {
+      responseHeader,
+      responseProjects
+    }
+  }
 }
 
 export default Index
