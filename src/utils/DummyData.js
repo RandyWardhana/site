fc6fb79ee12ef513a98ec2d3566b6ca0a8e37081
RandyWardhana <randywardhana37@gmail.com>
2020-08-08 18:05:38 +0700

    Create dummy data for skills

---
+++ src/utils/DummyData.js
@@ -0,0 +1,76 @@
+import { 
+  CSS, Discord, ExpressJS, Figma, HTML,
+  JavaScript, Laravel, MySQL, NextJS, NodeJS,
+  PhotoShop, PostgreSQL, ReactJS, Redux, TypeScript
+} from '../components/Svg'
+
+const SkillsData = (fill, fillSecondary) => {
+  return [
+    {
+      icon: <ReactJS fill={fill} />,
+      caption: 'React Native'
+    },
+    {
+      icon: <ReactJS fill={fill} />,
+      caption: 'React JS'
+    },
+    {
+      icon: <Redux fill={fill} />,
+      caption: 'Redux'
+    },
+    {
+      icon: <NextJS fill={fill} />,
+      caption: 'Next.js'
+    },
+    {
+      icon: <Discord fill={fill} />,
+      caption: 'Discord.js'
+    },
+    {
+      icon: <JavaScript fill={fill} fillSecondary={fillSecondary} />,
+      caption: 'JavaScript'
+    },
+    {
+      icon: <CSS fill={fill} fillSecondary={fillSecondary} />,
+      caption: 'CSS'
+    },
+    {
+      icon: <HTML fill={fill} fillSecondary={fillSecondary} />,
+      caption: 'HTML'
+    },
+    {
+      icon: <Figma fill={fill} />,
+      caption: 'Figma'
+    },
+    {
+      icon: <PhotoShop fill={fill} fillSecondary={fillSecondary} />,
+      caption: 'Adobe PhotoShop'
+    },
+    {
+      icon: <MySQL fill={fill} />,
+      caption: 'MySQL'
+    },
+    {
+      icon: <PostgreSQL fill={fill} fillSecondary={fillSecondary} />,
+      caption: 'PostgreSQL'
+    },
+    {
+      icon: <TypeScript fill={fill} fillSecondary={fillSecondary} />,
+      caption: 'TypeScript'
+    },
+    {
+      icon: <NodeJS fill={fill} />,
+      caption: 'Node.js'
+    },
+    {
+      icon: <ExpressJS fill={fill} />,
+      caption: 'Express.JS'
+    },
+    {
+      icon: <Laravel fill={fill} />,
+      caption: 'Laravel'
+    },
+  ]
+}
+
+export { SkillsData }
