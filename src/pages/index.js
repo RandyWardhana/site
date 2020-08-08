37616a5e3332417904ec7e900e363e7b8150f2b8
RandyWardhana <randywardhana37@gmail.com>
2020-08-08 20:05:16 +0700

    Use React Lazy & Suspense

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
-import Project from '../components/Project'
-import Skills from '../components/Skills'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
-import Layout from '../layout'
+const Navbar = lazy(() => import('../components/Navbar'))
+const Header = lazy(() => import('../components/Header'))
+const Project = lazy(() => import('../components/Project'))
+const Skills = lazy(() => import('../components/Skills'))
+const Contact = lazy(() => import('../components/Contact'))
+const Footer = lazy(() => import('../components/Footer'))
+const Layout = lazy(() => import('../layout'))
 
 import useWindowSize from '../utils/resizeWindowSize'
 import { getProjects, getHeader } from '../utils/FirebaseRequest'
 
+const renderLoader = () => <div style={styles.suspenseContainer}><span style={{ fontSize: 24 }}>Loading...</span></div>
+
 const Index = (props) => {
   const size = useWindowSize()
+  
+
   return (
-    <>
+    <Suspense fallback={renderLoader(props.theme)}>
       <Navbar currentPosition={'HOME'} size={size} theme={props.theme} toggle={props.toggle} />
       <Layout>
         <div style={{ margin: '60px auto 0', padding: size.width <= 768 ? '0 32px' : 0 }}>
@@ -25,14 +29,25 @@ const Index = (props) => {
           <Footer size={size} theme={props.theme} />
         </div>
       </Layout>
-    </>
+    </Suspense>
   )
 }
 
-export async function getStaticProps () {
+const styles = {
+  suspenseContainer: {
+    alignItems: 'center',
+    background: ({ theme }) => theme.background,
+    display: 'flex',
+    height: '100vh',
+    justifyContent: 'center',
+    width: '100%'
+  }
+}
+
+export async function getStaticProps() {
   const responseHeader = await getHeader()
   const responseProjects = await getProjects()
-  
+
   return {
     props: {
       responseHeader,
